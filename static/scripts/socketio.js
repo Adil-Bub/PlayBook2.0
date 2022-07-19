bc938c3c7c38d5393634a3603a86cdb539103285
Adil Ahmed <adilahmed1090@gmail.com>
2022-07-20 04:19:39 +0530

    last commit

--- static/scripts/socketio.js
+++ static/scripts/socketio.js
@@ -1,20 +1,20 @@
 document.addEventListener('DOMContentLoaded', () => {
     const socket = io();
 
-    socket.on('message', (data) => {
+    socket.on('message', (DATA) => {
         const card = document.createElement('div')
         card.classList.add('card', 'mb-2')
         const card_header = document.createElement('div')
         card_header.classList.add('card-header')
-        card_header.innerHTML = data[0]
+        card_header.innerHTML = DATA[0]
         const card_body = document.createElement('div')
         card_body.classList.add('card-body')
         const card_title = document.createElement('h4')
         card_title.classList.add('card-title')
-        card_title.innerHTML = data[1]
+        card_title.innerHTML = DATA[1]
         const card_text = document.createElement('p')
         card_text.classList.add('card-text')
-        card_text.innerHTML = data[2]
+        card_text.innerHTML = DATA[2]
         card_body.append(card_title, card_text)
         card.append(card_header, card_body)
         const parent = document.getElementById('posts')
